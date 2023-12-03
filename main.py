5c233b20128e3d6971c8705c336641f99bc56e8a
hendrik <hendrik@hbook.local>
2023-12-03 16:42:27 +0100

    proof of concept

---
+++ main.py
@@ -0,0 +1,42 @@
+import datetime
+from concurrent.futures import ThreadPoolExecutor
+
+import boto3
+from dateutil.relativedelta import relativedelta
+
+
+def copy_object(
+    s3_resource, source_bucket, destination_bucket, obj_key, destination_prefix
+):
+    copy_source = {"Bucket": source_bucket, "Key": obj_key}
+    destination_key = destination_prefix + obj_key
+    print(f"Copying {obj_key} to {destination_key}")
+    s3_resource.meta.client.copy(copy_source, destination_bucket, destination_key)
+
+
+def main():
+    source_bucket = "hendriks-second-brain-dev"
+    destination_bucket = "hendriks-second-brain-backup"
+    max_workers = (
+        1  # Adjust this number based on your requirements and system capabilities
+    )
+
+    s3_resource = boto3.resource("s3")
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    destination_prefix = f"{today}/"
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        bucket = s3_resource.Bucket(source_bucket)
+        for obj in bucket.objects.all():
+            executor.submit(
+                copy_object,
+                s3_resource,
+                source_bucket,
+                destination_bucket,
+                obj.key,
+                destination_prefix,
+            )
+
+
+if __name__ == "__main__":
+    main()
